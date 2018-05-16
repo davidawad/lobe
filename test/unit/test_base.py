c3c1bc8327aee9f870eb58ecfb402cf0f0f026c8
david awad <me@davidawad.com>
2018-05-16 17:53:30 -0400

    FEAT: renamed function for tests

--- test/unit/test_base.py
+++ test/unit/test_base.py
@@ -9,7 +9,7 @@ sys.path.append('../')
 
 from test_conf import *
 
-from processing import handle_parsed_intent
+from processing import extract_reply_from_intent
 from nlp_tools.proc_english import split_into_sentences
 
 
@@ -18,27 +18,27 @@ class TestBotProcessor(object):
 
     """
 
-    def test_handle_parsed_intent(self):
-        response_text = handle_parsed_intent("greetings")
+    def test_extract_reply_from_intent(self):
+        response_text = extract_reply_from_intent("greetings")
         assert response_text == "Hello! My name is Lobe, I'm a robot designed to help you, but I'm not an attorney! Any information I give is exclusively for entertainment purposes only."
 
         return
 
 
-    def test_handle_parsed_intent(self):
-        response_text = handle_parsed_intent("court_basics")
+    def test_extract_reply_from_intent(self):
+        response_text = extract_reply_from_intent("court_basics")
         assert "You will appear before" in response_text
 
         return
 
 
-    def test_handle_parsed_intent_bad_input(self):
-        response_text = handle_parsed_intent(None)
+    def test_extract_reply_from_intent_bad_input(self):
+        response_text = extract_reply_from_intent(None)
         assert "I'm sorry I didn't understand that!" in response_text
         return
 
-    def test_handle_parsed_intent_bad_input(self):
-        response_text = handle_parsed_intent('FAKEFAKE')
+    def test_extract_reply_from_intent_bad_input(self):
+        response_text = extract_reply_from_intent('FAKEFAKE')
         assert "I'm sorry I didn't understand that!" in response_text
         return
 
