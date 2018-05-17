1a8ff16b4371abe2302d1f4565bab7e604c6feaf
david awad <me@davidawad.com>
2018-05-17 18:33:24 -0400

    FEAT: small tune

--- test/unit/test_base.py
+++ test/unit/test_base.py
@@ -55,3 +55,16 @@ class TestBotNLP(object):
 
         return
 
+class TestWit(object):
+
+    # test entity parsing
+    # {'location': [{'suggested': True, 'confidence': 0.9186, 'value': 'jail', 'type': 'value'}], 'intent': [{'confidence': 0.53545161875344, 'value': 'trial'}]}
+
+    def test_handle_parsed_intent(self):
+        sentences = split_into_sentences("Hello! My name is Lobe, I'm a robot designed to help you, but I'm not an attorney! Any information I give is exclusively for entertainment purposes only.")
+
+        assert sentences == ['Hello!', "My name is Lobe, I'm a robot designed to help you, but I'm not an attorney!", 'Any information I give is exclusively for entertainment purposes only.']
+
+        return
+
+
