642ab2fad01e94196473fe847b446177eaac1c35
david awad <me@davidawad.com>
2018-07-26 12:31:07 -0400

    FIX: rename function imports for testing

--- test/unit/test_base.py
+++ test/unit/test_base.py
@@ -8,8 +8,7 @@ import sys
 sys.path.append('../')
 
 from test_conf import *
-
-from processing import extract_reply_from_intent
+from processing import determine_reply_from_intent
 from nlp_tools.proc_english import split_into_sentences
 
 
@@ -18,27 +17,27 @@ class TestBotProcessor(object):
 
     """
 
-    def test_extract_reply_from_intent(self):
-        response_text = extract_reply_from_intent("greetings")
+    def test_determine_reply_from_intent(self):
+        response_text = determine_reply_from_intent("greetings")
         assert response_text == "Hello! My name is Lobe, I'm a robot designed to help you, but I'm not an attorney! Any information I give is exclusively for entertainment purposes only."
 
         return
 
 
-    def test_extract_reply_from_intent(self):
-        response_text = extract_reply_from_intent("court_basics")
+    def test_determine_reply_from_intent(self):
+        response_text = determine_reply_from_intent("court_basics")
         assert "You will appear before" in response_text
 
         return
 
 
-    def test_extract_reply_from_intent_bad_input(self):
-        response_text = extract_reply_from_intent(None)
+    def test_determine_reply_from_intent_bad_input(self):
+        response_text = determine_reply_from_intent(None)
         assert "I'm sorry I didn't understand that!" in response_text
         return
 
-    def test_extract_reply_from_intent_bad_input(self):
-        response_text = extract_reply_from_intent('FAKEFAKE')
+    def test_determine_reply_from_intent_bad_input(self):
+        response_text = determine_reply_from_intent('FAKEFAKE')
         assert "I'm sorry I didn't understand that!" in response_text
         return
 
--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -9,12 +9,10 @@ unit tests for utils
 import sys
 sys.path.append('../')
 
-from test_conf import *
-
-from processing import extract_reply_from_intent
 from nlp_tools.proc_english import split_into_sentences
-
+from processing import determine_reply_from_intent
 from utils import find_state_from_coords
+from test_conf import *
 
 
 
@@ -30,5 +28,19 @@ class TestUtils(object):
         """
         assert find_state_from_coords(37.483872693672, -122.14900441942) == 'CA'
         assert find_state_from_coords(40.483872693672, -73.9321059) == 'NY'
+
+    # def test_find_state(self):
+
+    #  @fixture(scope='function')
+    #  def stuff():
+        #  global counter
+        #  counter = 0
+
+
+    #  @given(a=st.none())
+    #  def test_stuff(a, stuff):
+        #  global counter
+        #  counter += 1
+        #  assert counter == 1
 
 
